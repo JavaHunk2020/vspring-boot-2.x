d29b4caa2221eba09c969fade136b196c6b1133a
Nagendra Kumar <nagendra.kumar@controlmap.io>
2023-03-24 03:32:32 +0530

    comment is added on main file

--- src/main/java/com/rab3tech/MainStartBoot.java
+++ src/main/java/com/rab3tech/MainStartBoot.java
@@ -2,7 +2,11 @@ package com.rab3tech;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * 
+ * @author LENOVO Nagendra Kumar
+ *
+ */
 @SpringBootApplication
 public class MainStartBoot {
 	
